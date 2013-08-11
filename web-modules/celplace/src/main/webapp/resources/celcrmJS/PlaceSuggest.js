db58b3a444bc89460a660e321952ceb147b2fa0d
Fabian Pichler <fabian.pichler@synventis.com>
2013-08-12 00:57:59 +0200

    add getSuggestPlaces to PlaceSuggest.js

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
@@ -23,6 +23,38 @@ CELEMENTS.CRM.place.Suggest = function(cssSelector) {
       _init : function(cssSelector) {
         var _me = this;
         _me._placeFields = $$(cssSelector);
+      },
+
+      getSuggestPlaces : function() {
+        var _me = this;
+        var params = {
+            'xpage' : 'celements_ajax',
+            'ajax_mode' : 'SuggestPlace'
+         };
+        _me._placeFields.each(function(inputField) {
+          params[inputField.name] = inputField.value;
+        });
+        new Ajax.Request(getCelHost(), {
+          method: 'post',
+          parameters: params,
+          onSuccess: function(transport) {
+            if (transport.responseText.isJSON()) {
+              //TODO
+              console.log('getSuggestPlaces: success ', transport.responseText);
+            } else if ((typeof console != 'undefined')
+                && (typeof console.warn != 'undefined')) {
+              console.warn('getSuggestPlaces: noJSON!!! ', transport.responseText);
+            }
+          },
+          onFailure : function(transport) {
+            if ((typeof console != 'undefined')
+                && (typeof console.warn != 'undefined')) {
+              console.warn('getSuggestPlaces: failed to execute request for'
+                + ' params [' + Object.toJSON(params) + ']: ', transport.status,
+                transport.statusText);
+            }
+          }
+        });
       }
 
   };

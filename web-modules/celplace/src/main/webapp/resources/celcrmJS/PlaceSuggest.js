704cdb52bb6473be8b1d0d110ddfc6ee94792467
Fabian Pichler <fabian.pichler@synventis.com>
2013-08-12 08:53:44 +0200

    add callbackFunc parameter to getSuggestPlaces and add event for successful result of ajax call in PlaceSuggest.js

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
@@ -19,42 +19,52 @@ CELEMENTS.CRM.place.Suggest = function(cssSelector) {
 (function() {
   CELEMENTS.CRM.place.Suggest.prototype = {
       _placeFields : undefined,
+      _hasOpenRequest : false,
 
       _init : function(cssSelector) {
         var _me = this;
         _me._placeFields = $$(cssSelector);
       },
 
-      getSuggestPlaces : function() {
+      getSuggestPlaces : function(callbackFunc) {
         var _me = this;
-        var params = {
-            'xpage' : 'celements_ajax',
-            'ajax_mode' : 'SuggestPlace'
-         };
-        _me._placeFields.each(function(inputField) {
-          params[inputField.name] = inputField.value;
-        });
-        new Ajax.Request(getCelHost(), {
-          method: 'post',
-          parameters: params,
-          onSuccess: function(transport) {
-            if (transport.responseText.isJSON()) {
-              //TODO
-              console.log('getSuggestPlaces: success ', transport.responseText);
-            } else if ((typeof console != 'undefined')
-                && (typeof console.warn != 'undefined')) {
-              console.warn('getSuggestPlaces: noJSON!!! ', transport.responseText);
+        if (!_me._hasOpenRequest) {
+          _me._hasOpenRequest = true;
+          var params = {
+              'xpage' : 'celements_ajax',
+              'ajax_mode' : 'SuggestPlace'
+           };
+          _me._placeFields.each(function(inputField) {
+            params[inputField.name] = inputField.value;
+          });
+          new Ajax.Request(getCelHost(), {
+            method: 'post',
+            parameters: params,
+            onSuccess: function(transport) {
+              if (transport.responseText.isJSON()) {
+                var responseObj = transport.responseText.evalJSON();
+                _me._placeFields[0].up('form').fire('celcrm:PlaceSuggest', responseObj);
+                if (callbackFunc) {
+                  callbackFunc(responseObj);
+                }
+              } else if ((typeof console != 'undefined')
+                  && (typeof console.warn != 'undefined')) {
+                console.warn('getSuggestPlaces: noJSON!!! ', transport.responseText);
+              }
+            },
+            onFailure : function(transport) {
+              if ((typeof console != 'undefined')
+                  && (typeof console.warn != 'undefined')) {
+                console.warn('getSuggestPlaces: failed to execute request for'
+                  + ' params [' + Object.toJSON(params) + ']: ', transport.status,
+                  transport.statusText);
+              }
+            },
+            onComplete : function(transport) {
+              _me._hasOpenRequest = false;
             }
-          },
-          onFailure : function(transport) {
-            if ((typeof console != 'undefined')
-                && (typeof console.warn != 'undefined')) {
-              console.warn('getSuggestPlaces: failed to execute request for'
-                + ' params [' + Object.toJSON(params) + ']: ', transport.status,
-                transport.statusText);
-            }
-          }
-        });
+          });
+        }
       }
 
   };

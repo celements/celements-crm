dea1550b8ac4a74460b84b3fd43e63c3e1fb657a
Fabian Pichler <fabian.pichler@synventis.com>
2013-08-28 14:06:11 +0200

    fix getSuggestPlaces skipps Ajax request if already one request is pending

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/PlaceSuggest.js
@@ -19,7 +19,7 @@ CELEMENTS.CRM.place.Suggest = function(cssSelector) {
 (function() {
   CELEMENTS.CRM.place.Suggest.prototype = {
       _placeFields : undefined,
-      _hasOpenRequest : false,
+      _suggestPlaceRequest : null,
 
       _init : function(cssSelector) {
         var _me = this;
@@ -28,43 +28,44 @@ CELEMENTS.CRM.place.Suggest = function(cssSelector) {
 
       getSuggestPlaces : function(callbackFunc) {
         var _me = this;
-        if (!_me._hasOpenRequest) {
-          _me._hasOpenRequest = true;
-          var params = {
-              'xpage' : 'celements_ajax',
-              'ajax_mode' : 'SuggestPlace'
-           };
-          _me._placeFields.each(function(inputField) {
-            params[inputField.name] = inputField.value;
-          });
-          new Ajax.Request(getCelHost(), {
-            method: 'post',
-            parameters: params,
-            onSuccess: function(transport) {
-              if (transport.responseText.isJSON()) {
-                var responseObj = transport.responseText.evalJSON();
-                _me._placeFields[0].up('form').fire('celcrm:PlaceSuggest', responseObj);
-                if (callbackFunc) {
-                  callbackFunc(responseObj);
-                }
-              } else if ((typeof console != 'undefined')
-                  && (typeof console.warn != 'undefined')) {
-                console.warn('getSuggestPlaces: noJSON!!! ', transport.responseText);
-              }
-            },
-            onFailure : function(transport) {
-              if ((typeof console != 'undefined')
-                  && (typeof console.warn != 'undefined')) {
-                console.warn('getSuggestPlaces: failed to execute request for'
-                  + ' params [' + Object.toJSON(params) + ']: ', transport.status,
-                  transport.statusText);
-              }
-            },
-            onComplete : function(transport) {
-              _me._hasOpenRequest = false;
-            }
-          });
+        var params = {
+            'xpage' : 'celements_ajax',
+            'ajax_mode' : 'SuggestPlace'
+         };
+        _me._placeFields.each(function(inputField) {
+          params[inputField.name] = inputField.value;
+        });
+        if (_me._suggestPlaceRequest) {
+          _me._suggestPlaceRequest.transport.abort();
+          _me._suggestPlaceRequest = null;
         }
+        _me._suggestPlaceRequest = new Ajax.Request(getCelHost(), {
+          method: 'post',
+          parameters: params,
+          onSuccess: function(transport) {
+            if (transport.responseText.isJSON()) {
+              var responseObj = transport.responseText.evalJSON();
+              _me._placeFields[0].up('form').fire('celcrm:PlaceSuggest', responseObj);
+              if (callbackFunc) {
+                callbackFunc(responseObj);
+              }
+            } else if ((typeof console != 'undefined')
+                && (typeof console.warn != 'undefined')) {
+              console.warn('getSuggestPlaces: noJSON!!! ', transport.responseText);
+            }
+          },
+          onFailure : function(transport) {
+            if ((typeof console != 'undefined')
+                && (typeof console.warn != 'undefined')) {
+              console.warn('getSuggestPlaces: failed to execute request for'
+                + ' params [' + Object.toJSON(params) + ']: ', transport.status,
+                transport.statusText);
+            }
+          },
+          onComplete : function(transport) {
+            _me._suggestPlaceRequest = null;
+          }
+        });
       }
 
   };

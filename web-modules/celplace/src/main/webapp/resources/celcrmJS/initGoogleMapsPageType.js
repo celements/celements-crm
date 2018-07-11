7ff6b5ff2dbf8fc973c54c10b4b4de43cdfc5b85
Dari Khasham <dari.khasham@synventis.com>
2018-07-11 17:32:43 +0200

    change zoomfactor

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.js
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.js
@@ -5,15 +5,10 @@
     var mapContainer = $("googleMapsContainer");
     var longitude = parseFloat(mapContainer.getAttribute("data-longitude"));
     var latitude = parseFloat(mapContainer.getAttribute("data-latitude"));
-    console.log("<<<<<<<<< initGoogleMapsPageType initGoogleMaps longitude: ", longitude);
-    console.log("<<<<<<<<< initGoogleMapsPageType initGoogleMaps latitude: ", latitude);
     
-    // The location of Uluru
-    var uluru = {lat: latitude, lng: longitude};
-    // The map, centered at Uluru
-    var map = new google.maps.Map($('googleMapsContainer'), {zoom: 4, center: uluru});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    var place = {lat: latitude, lng: longitude};
+    var map = new google.maps.Map($('googleMapsContainer'), {zoom: 15, center: place});
+    var marker = new google.maps.Marker({position: place, map: map});
 
   };
   

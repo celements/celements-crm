2080d4d977fc1c60b6591900df518021c7262251
Fabian Pichler <pichlerf@SYNWS02.synventis.com>
2018-09-01 00:07:56 +0200

    update Test

--- components/celplace/src/test/java/com/celements/crm/place/geocoding/GeocodingServiceTest.java
+++ components/celplace/src/test/java/com/celements/crm/place/geocoding/GeocodingServiceTest.java
@@ -1,5 +1,6 @@
 package com.celements.crm.place.geocoding;
 
+import static com.celements.common.test.CelementsTestUtils.*;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
@@ -7,11 +8,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.xwiki.configuration.ConfigurationSource;
 
-import com.celements.common.test.AbstractBridgedComponentTestCase;
+import com.celements.common.test.AbstractComponentTest;
 import com.google.maps.GeoApiContext;
 import com.xpn.xwiki.web.Utils;
 
-public class GeocodingServiceTest extends AbstractBridgedComponentTestCase {
+public class GeocodingServiceTest extends AbstractComponentTest {
 
   private GeocodingService geocodingService;
 
@@ -31,13 +32,13 @@ public class GeocodingServiceTest extends AbstractBridgedComponentTestCase {
   public void testGetGMapsContext() throws Exception {
     String apiKey = "asdf";
 
-    expect(confSrcMock.getProperty(eq("celements.geocoding.googlemaps.apikey"),
-        same(String.class))).andReturn(apiKey).anyTimes();
+    expect(confSrcMock.getProperty(eq("celements.geocoding.googlemaps.apikey"), same(
+        String.class))).andReturn(apiKey).anyTimes();
     expect(gMapsContextMock.setApiKey(eq(apiKey))).andReturn(gMapsContextMock).once();
-    expect(confSrcMock.getProperty(eq("celements.geocoding.googlemaps.qps"), eq(2))
-        ).andReturn(2).anyTimes();
+    expect(confSrcMock.getProperty(eq("celements.geocoding.googlemaps.qps"), eq(2))).andReturn(
+        2).anyTimes();
     expect(gMapsContextMock.setQueryRateLimit(eq(2))).andReturn(gMapsContextMock).once();
-    
+
     replayDefault();
     assertSame(gMapsContextMock, geocodingService.getGMapsContext());
     verifyDefault();

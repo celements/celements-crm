d80d9a03ff093b7f3a1021d9501950a202ba5aab
Marc Sladek <marc.sladek@synventis.com>
2019-03-21 01:37:37 +0100

    add ajax geocodeAddress

---
+++ components/celplace/src/main/java/com/celements/crm/place/geocoding/GeocodingScriptService.java
@@ -0,0 +1,43 @@
+package com.celements.crm.place.geocoding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+import org.xwiki.script.service.ScriptService;
+
+import com.celements.model.classes.ClassDefinition;
+import com.celements.rights.access.IRightsAccessFacadeRole;
+import com.google.maps.model.LatLng;
+
+@Component("geocoding")
+public class GeocodingScriptService implements ScriptService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GeocodingScriptService.class);
+
+  @Requirement
+  private IGeocodingServiceRole geocodingService;
+
+  @Requirement
+  private IRightsAccessFacadeRole rightsAccess;
+
+  public List<LatLng> geocodeAddress(String address) {
+    List<LatLng> ret = new ArrayList<>();
+    if (rightsAccess.isLoggedIn()) {
+      try {
+        ret = geocodingService.geocodeAddress(address);
+      } catch (GeocodingException exc) {
+        LOGGER.warn("geocodeAddress - failed for [{}]", address, exc);
+      }
+    }
+    return ret;
+  }
+
+  public ClassDefinition getGeotagClass() {
+    return geocodingService.getGeotagClass();
+  }
+
+}

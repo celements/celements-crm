bb6dda1e3d0143cb37ae2c1109e96953189a90a5
Fabian Pichler <fabian.pichler@synventis.com>
2022-09-27 14:47:52 +0200

    add XWikiPreferences parameter and MetaTag for GoogleMapsApiKey

---
+++ components/celplace/src/main/java/com/celements/crm/place/GoogleMapsApiKeyMetaTagService.java
@@ -0,0 +1,49 @@
+package com.celements.crm.place;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+import org.apache.commons.lang.StringUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.Requirement;
+
+import com.celements.metatag.MetaTag;
+import com.celements.metatag.MetaTagProviderRole;
+import com.celements.model.context.ModelContext;
+import com.google.common.collect.ImmutableList;
+
+@Component(GoogleMapsApiKeyMetaTagService.NAME)
+public class GoogleMapsApiKeyMetaTagService implements MetaTagProviderRole {
+
+  public static final String NAME = "GoogleMapsApiKeyMetaTag";
+
+  public static final String CEL_GOOGLE_MAPS_API_KEY = "celGoogleMapsApiKey";
+
+  @Requirement
+  private ModelContext context;
+
+  @Override
+  public @NotNull List<MetaTag> getBodyMetaTags() {
+    return Collections.emptyList();
+  }
+
+  boolean hasGoogleMapsApiKey() {
+    return !StringUtils.isEmpty(getGoogleMapsApiKey());
+  }
+
+  String getGoogleMapsApiKey() {
+    return context.getXWikiContext().getWiki().getXWikiPreference(CEL_GOOGLE_MAPS_API_KEY, "",
+        context.getXWikiContext());
+  }
+
+  @Override
+  public @NotNull List<MetaTag> getHeaderMetaTags() {
+    if (hasGoogleMapsApiKey()) {
+      return ImmutableList.of(new MetaTag("name", "cel-GMaps-ApiKey", getGoogleMapsApiKey()));
+    }
+    return Collections.emptyList();
+  }
+
+}

e6fe62f2d54f1018424c2240283bea47dfeeadd8
Fabian Pichler <fabian.pichler@synventis.com>
2022-09-28 08:05:28 +0200

    add getGeoTagClassRef to crmplace script service

--- components/celplace/src/main/java/com/celements/crm/place/PlaceScriptService.java
+++ components/celplace/src/main/java/com/celements/crm/place/PlaceScriptService.java
@@ -2,6 +2,7 @@ package com.celements.crm.place;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
+import org.xwiki.model.reference.ClassReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.crm.place.geocoding.IGeocodingServiceRole;
@@ -13,6 +14,14 @@ public class PlaceScriptService implements ScriptService {
   @Requirement
   private IGeocodingServiceRole geocodingService;
 
+  public ClassReference getGeoTagClassRef() {
+    return geocodingService.getGeotagClass().getClassReference();
+  }
+
+  /**
+   * @deprecated since 5.5 instead use getGeoTagClassRef
+   */
+  @Deprecated
   public ClassDefinition getGeotagClassDefinition() {
     return geocodingService.getGeotagClass();
   }

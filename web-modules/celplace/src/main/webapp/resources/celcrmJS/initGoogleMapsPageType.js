42ff095a8a31a414674242221103cd51959055c3
Fabian Pichler <fabian.pichler@synventis.com>
2022-09-29 08:01:35 +0200

    change initGoogleMapsPageType.js to automatically add the googleapis javascripts

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.js
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.js
@@ -1,19 +1,34 @@
 (function(window, undefined) {
   "use strict";
   
-  var initGoogleMaps = function() {
-    var mapContainer = $("googleMapsContainer");
-    var longitude = parseFloat(mapContainer.getAttribute("data-longitude"));
-    var latitude = parseFloat(mapContainer.getAttribute("data-latitude"));
-    
-    var place = {lat: latitude, lng: longitude};
-    var map = new google.maps.Map($('googleMapsContainer'), {zoom: 15, center: place});
-    var marker = new google.maps.Marker({position: place, map: map});
-
+  const initLoadMap = function() {
+    const mapContainer = $("googleMapsContainer");
+    const longitude = parseFloat(mapContainer.getAttribute("data-longitude"));
+    const latitude = parseFloat(mapContainer.getAttribute("data-latitude"));
+    const place = {lat: latitude, lng: longitude};
+    const map = new google.maps.Map($('googleMapsContainer'), {zoom: 15, center: place});
+    const marker = new google.maps.Marker({position: place, map: map});
   };
   
-  celAddOnBeforeLoadListener(function() {
-    initGoogleMaps();
-  });
-  
+  const initializeGoogleMaps = function() {
+    const metas = document.querySelectorAll('meta[name="cel-GMaps-ApiKey"]');
+    if ((metas.length > 0) && (metas[0].content !== '')) {
+      const gMapsApiKey = metas[0].content;
+      const gmapJsElem = document.createElement('script');
+      gmapJsElem.src = 'https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false'
+        + '&key=' + gMapsApiKey;
+      gmapJsElem.type = 'text/javascript';
+      gmapJsElem.addEventListener('load', initLoadMap);
+      document.head.appendChild(gmapJsElem);
+    } else {
+      console.warn('Cannot initialize Google Maps, because of Google Maps Api key is missing.');
+    }
+  };
+
+  if (['complete', 'interactive'].includes(document.readyState)) {
+    initializeGoogleMaps();
+  } else {
+    document.addEventListener('DOMContentLoaded', initializeGoogleMaps);
+  }
+
 })(window);

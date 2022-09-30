2c863f9dd52d7a5cd5a4cbf1f37c0056d4fc1a9a
Fabian Pichler <fabian.pichler@synventis.com>
2022-09-30 14:39:34 +0200

    fix versioning

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.mjs
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.mjs
@@ -1,7 +1,4 @@
-const version = new URL(import.meta.url).searchParams.get('someURLInfo')
-console.log("initGoogleMapsPageType.js version ", version);
-
-import CelGoogleMapsViewer from ("./CelGoogleMapsViewer.mjs?version=" + version);
+import CelGoogleMapsViewer from "./CelGoogleMapsViewer.mjs?version=202209301438";
 
 if (['complete', 'interactive'].includes(document.readyState)) {
   console.debug("initGoogleMaps in dynamic mode loaded immediately", document.readyState);

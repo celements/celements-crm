388f57bdff0122606cf8d79fdaad3d5ddba149c9
Fabian Pichler <fabian.pichler@synventis.com>
2022-10-14 10:28:48 +0200

    change to onReady

--- web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.mjs
+++ web-modules/celplace/src/main/webapp/resources/celcrmJS/initGoogleMapsPageType.mjs
@@ -1,10 +1,7 @@
 import { CelGoogleMapsViewer } from "./CelGoogleMapsViewer.mjs?version=202210100633";
 
-const googleMapViewer = new CelGoogleMapsViewer();
-if (['complete', 'interactive'].includes(document.readyState)) {
-  console.debug("initGoogleMaps in dynamic mode loaded immediately", document.readyState);
-  googleMapViewer.load();
-} else {
-  console.debug("initGoogleMaps in sync mode loaded on defer ", document.readyState);
-  document.addEventListener('DOMContentLoaded', () => googleMapViewer.load());
-}
+const onReady = callback => (document.readyState === 'loading')
+    ? document.addEventListener('DOMContentLoaded', callback)
+    : callback();
+
+onReady(() => new CelGoogleMapsViewer().load());

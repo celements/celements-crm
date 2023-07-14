f42c79560c22875e5c0da62aa880059572cae77d
Marc Sladek <marc@sladek.dev>
2023-07-14 09:38:59 +0200

    remove getGeotagClassDefinition (#31)

--- components/celplace/src/main/java/com/celements/crm/place/PlaceScriptService.java
+++ components/celplace/src/main/java/com/celements/crm/place/PlaceScriptService.java
@@ -8,7 +8,6 @@ import org.xwiki.model.reference.ClassReference;
 import org.xwiki.script.service.ScriptService;
 
 import com.celements.crm.place.geocoding.IGeocodingServiceRole;
-import com.celements.model.classes.ClassDefinition;
 
 @Component("crmplace")
 public class PlaceScriptService implements ScriptService {
@@ -23,21 +22,6 @@ public class PlaceScriptService implements ScriptService {
       return geocodingService.getGeotagClass().getClassReference();
     } catch (Exception exc) {
       LOGGER.info("Failed to get getGeoTagClassRef.", exc);
-      return null;
-    }
-  }
-
-  /**
-   * @deprecated since 5.5 instead use getGeoTagClassRef
-   */
-  @Deprecated
-  public ClassDefinition getGeotagClassDefinition() {
-    LOGGER.warn("Deprecated getGeotagClassDefinition is still used. This might be an security"
-        + "issue.");
-    try {
-      return geocodingService.getGeotagClass();
-    } catch (Exception exc) {
-      LOGGER.info("Failed to get getGeotagClassDefinition.", exc);
       return null;
     }
   }
